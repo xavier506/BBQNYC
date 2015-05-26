33899f75468bde6953801b6c3c58c12385c05032
Julia Becker <jcbecker26@gmail.com>
2015-05-26 17:36:19 -0400

    create event form

--- BBQ-NYC/app/assets/javascripts/scripts.js
+++ BBQ-NYC/app/assets/javascripts/scripts.js
@@ -30,36 +30,91 @@ $(function() {
   });
 
   var Event = Backbone.Model.extend({
-    urlRoot: '/api/events'
-  })
+    urlRoot: 'api/events'
+  });
+
+  var User = Backbone.Model.extend({
+    initialize: function(attributes, options) {
+      this.event_id = options.event_id;
+    },
+    urlRoot: function() {
+      return '/api/events/' + this.event_id + '/users'
+    }
+  });
 
   var EventCollection = Backbone.Collection.extend({
-
+    url: 'api/events',
+    model: Event
   });
 
   var EventFormView = Backbone.View.extend({
-    initialize: function(options) {
-      this.eventCollection = options.collection
-    },
+    el: $('#main'),
     template: $('script[data-id="create-event-view"]').text(),
+    events: {
+      'submit': 'createEvent'
+    },
     render: function() {
       this.$el.html(_.template(this.template))
+    },
+    createEvent: function(event) {
+      event.preventDefault();
+      var host_name = $('[name="host-name"]').val();
+      var host_email = $('[name="email"]').val();
+      var name = $('[name="event-name"]').val();
+      var date = $('#datepicker').val();
+      var description = $('[name="description"]').val();
+
+      // Remove "Time: " label from time value
+      var time = $('.slider-time').html();
+      time = time.slice(6, time.length)
+
+      // Hashtags stored without # characters
+      var hashtag = $('[name="hashtag"]').val();
+      if (hashtag.charAt(0) === "#") {
+        hashtag = hashtag.slice(1, hashtag.length)
+      }
+
+      var newEvent = {
+        name: name,
+        hashtag: hashtag,
+        date: date,
+        description: description,
+        time: time
+      }
+      var e = new Event(newEvent);
+
+      e.save(newEvent, { 
+        success: function() {
+          var u = new User({name: host_name, email: host_email}, {
+            event_id: e.get("id")});
+          console.log(e.get("id"))
+          u.save();
+        }
+      });
+
+
+      // this.eventCollection.create(newEvent);
+
     }
   });
 
   var Router = Backbone.Router.extend({
     routes: {
       '': 'index',
-      'create': 'createEvent'
+      'create': 'createFormView'
     },
     index: function() {
       console.log("index hit")
-
     },
-    createEvent: function() {
+    createFormView: function() {
       console.log("create event hit")
+
+      var eventModel = new Event();
+
+      var eventCollection = new EventCollection();
       var formView = new EventFormView({
-        el: $("#main")
+        // el: $("#main")
+        // collection: eventCollection
       });
       formView.render();
     }
@@ -68,76 +123,47 @@ $(function() {
   var myRouter = new Router()
   Backbone.history.start()
 
-// Notes:
-
-// MODELS:
-// - Location
-// - Event
-// - User
-// - Supply
-
-// COLLECTIONS:
-// - Locations
-// - Users
-// - Supplies
-
-// MODEL VIEWS:
-// - Event
-// - Location
-// - Supply
-// - User
-
-// COLLECTION MODEL VIEWS:
-// - Locations
-// - Supplies
-// - Users
-
-// ROUTES
-// - ''
-// - '#create'
-// - '#events/:id'
-
-
-$(function() {
-      $( "#datepicker" ).datepicker();
-    });
-
-    $(function() {
-      $( "#slider" ).slider({
-        animate: "slow",
-        min: 480,
-        max: 1320,
-        step: 15,
-        value: 840,
-        slide: function (e, ui) {
-            var hours = Math.floor(ui.value / 60);
-            var minutes = ui.value - (hours * 60);
-            if (hours.length == 1) hours = '0' + hours;
-            if (minutes.length == 1) minutes = '0' + minutes;
-            if (minutes == 0) minutes = '00';
-            if (hours >= 12) {
-                if (hours == 12) {
-                    hours = hours;
-                    minutes = minutes + " PM";
-                } else {
-                    hours = hours - 12;
-                    minutes = minutes + " PM";
-                }
-            } else {
-                hours = hours;
-                minutes = minutes + " AM";
-            }
-            if (hours == 0) {
-                hours = 12;
-                minutes = minutes;
-            }
-            $('.slider-time').html('Time: ' + hours + ':' + minutes);
-        }
-      });
-    });
 
   $(function() {
-    $( "#radio" ).buttonset();
+    $("#datepicker").datepicker();
   });
 
+  $(function() {
+    $("#slider").slider({
+      animate: "slow",
+      min: 480,
+      max: 1320,
+      step: 15,
+      value: 840,
+      slide: function(e, ui) {
+        var hours = Math.floor(ui.value / 60);
+        var minutes = ui.value - (hours * 60);
+        if (hours.length == 1) hours = '0' + hours;
+        if (minutes.length == 1) minutes = '0' + minutes;
+        if (minutes == 0) minutes = '00';
+        if (hours >= 12) {
+          if (hours == 12) {
+            hours = hours;
+            minutes = minutes + " PM";
+          } else {
+            hours = hours - 12;
+            minutes = minutes + " PM";
+          }
+        } else {
+          hours = hours;
+          minutes = minutes + " AM";
+        }
+        if (hours == 0) {
+          hours = 12;
+          minutes = minutes;
+        }
+        $('.slider-time').html('Time: ' + hours + ':' + minutes);
+      }
+    });
   });
+
+  $(function() {
+    $("#radio").buttonset();
+  });
+
+});
--- BBQ-NYC/app/controllers/api/users_controller.rb
+++ BBQ-NYC/app/controllers/api/users_controller.rb
@@ -1,4 +1,18 @@
 module Api
   class UsersController < ApplicationController
+
+    def create
+      @user = User.find_or_initialize_by({email: params[:email]})
+      @user.name = params[:name]
+
+      @user.save
+
+      @ue = UserEvent.create({user_id: @user.id, event_id: params[:event_id]})
+
+      # render json: @ue.to_json
+
+      render json: @ue
+    end
+
   end
 end
