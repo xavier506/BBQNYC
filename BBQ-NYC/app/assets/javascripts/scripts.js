b445e71aaa5c6292f95be00ffb2ba09e4ff8c1be
Xavier Fernandez <xavier@crservers.com>
2015-05-27 00:03:26 -0400

    added park info to json file, hours images etc.

--- BBQ-NYC/app/assets/javascripts/scripts.js
+++ BBQ-NYC/app/assets/javascripts/scripts.js
@@ -43,7 +43,7 @@ $(function() {
     function infoWindow(marker, map, park) {
 
       google.maps.event.addListener(marker, 'click', function() {
-        var html = "<div><img src='"+park.photo_url+"' /><h3>" + park.name + "</h3><p>location: "+ park.location +"<br/><em>address: "+ park.address +"</em></p><p>description: "+ park.description +"</p><p>hours:"+ park.hours +"</p><p>rating: "+ park.rating +"</p><p><a href='"+ park.website + "' target='_blank'>visit park website</a></p><button>Grill Here</button></div>";
+        var html = "<div><img src='"+ park.photo_url +"' /><h3>" + park.name + "</h3><p>location: "+ park.location +"<br/><em>address: "+ park.address +"</em></p><p>description: "+ park.description +"</p><p>hours: "+ park.hours +"</p><p>rating: "+ park.rating +"</p><p><a href='"+ park.website + "' target='_blank'>visit park website</a></p><button>Grill Here</button></div>";
         iw = new google.maps.InfoWindow({
           content: html,
           maxWidth: 350
